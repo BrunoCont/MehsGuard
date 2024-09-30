3717890ecc55ffc931b3d395f15fdc411bd0cf8d
BrunoCont <bruno.cont288@gmail.com>
2024-09-30 10:52:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,19 @@
+// Instalação do Service Worker
 self.addEventListener('install', function(event) {
     console.log('Service Worker instalado.');
+    // Precache de recursos, se necessário
 });
 
+// Ativação do Service Worker
 self.addEventListener('activate', function(event) {
     console.log('Service Worker ativado.');
+});
+
+// Interceptando requisições de rede
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return new Response('Offline', { status: 404 }); // Resposta se estiver offline
+        })
+    );
 });
